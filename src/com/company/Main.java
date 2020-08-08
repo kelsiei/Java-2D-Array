ddf42a7c5a07194f85ec8395160d52712100aa94
kelsiei <kmmasti@gmail.com>
2020-08-08 11:32:14 -0400

    initial version

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        final int NUMBER_OF_ITEMS = 10;
+        int[] validValues = {101, 108, 201, 213, 266,
+                304, 311, 409, 411, 412};
+        double[] prices = {0.29, 1.23, 3.50, 0.69, 6.79,
+                3.19, 0.99, 0.89, 1.26, 8.00};
+        String strItem;
+        Boolean Valid = true;
+        int ans;
+        strItem = JOptionPane.showInputDialog(null, "Enter item number");
+        ans = 
+        for(int i = 0; i<NUMBER_OF_ITEMS; i++) {
+            if(validValues == )
+        }
+    }
+}
